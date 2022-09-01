524308ffc5153231d2fde162a5090621311dd23a
tewhitesca42 <tewhitesca42@login.localdomain>
2022-09-01 16:07:12 -0500

    working on finetune code

--- DL_Team/finetune/finetune_test.py
+++ DL_Team/finetune/finetune_test.py
@@ -1,6 +1,7 @@
 import numpy                      as np
 import tensorflow                 as tf
 import tensorflow_datasets        as tfds
+from   sklearn.preprocessing      import LabelBinarizer
 from   tensorflow.keras.callbacks import ModelCheckpoint
 from   keras.optimizers           import RMSprop
 from   keras.optimizers           import SGD
@@ -9,50 +10,55 @@ from   keras.layers               import Input
 from   tensorflow                 import keras
 from   keras.models               import Model
 
-(train_ds, validation_ds, test_ds) = tfds.load(
-    "cats_vs_dogs",
-    split=["train[:40%]", "train[40%:50%]","train[50%:60%]"],
+
+print("LOADING DATASET")
+((trainx,trainy),(testx,testy)) = tfds.as_numpy(tfds.load(
+    "tf_flowers",
+    split=["train[:40]", "train[40%:60%]"],
+    batch_size=-1,
     as_supervised=True
-)
+))
 
-size = (224,224)
+trainx = np.resize(trainx, (240,240))
+trainy = np.resize(trainy, (240,240))
+testx = np.resize(testx, (240,240))
+testy = np.resize(testy, (240,240))
 
-train_ds      = train_ds.map(lambda x, y: (tf.image.resize(x, size), y))
-validation_ds = validation_ds.map(lambda x, y: (tf.image.resize(x, size), y))
-test_ds      = test_ds.map(lambda x, y: (tf.image.resize(x, size), y))
-
-batch_size = 32
-
-train_ds      = train_ds.cache().batch(batch_size).prefetch(buffer_size=10)
-validation_ds = validation_ds.cache().batch(batch_size).prefetch(buffer_size=10)
-test_ds       = test_ds.cache().batch(batch_size).prefetch(buffer_size=10)
-
+print(trainx.shape)
+print("Here")
+trainx = trainx.astype("float") / 255.0
+testx = testx.astype("float") / 255.0
+print("Here1")
+#lb = LabelBinarizer()
+#trainy = lb.fit_transform(trainy)
+#testy = lb.transform(testy)
+print("Here2")
 data_augmentation = keras.Sequential(
     [keras.layers.RandomFlip("horizontal"), keras.layers.RandomRotation(0.1),]
 )
-
+print("Here3")
 base_model = keras.applications.VGG16(
     weights     = 'imagenet',
-    input_tensor = Input(shape=(224,224,3))
+    input_tensor = Input(shape=(240,240,3)),
     include_top = False # do not include Imagenet classifer
 )
-
+print("Here4")
 # freeze the base model
-head = OutputLayers.build(D=256, baseModel=base_model, numClasses=2)
+head = OutputLayers.build(D=256, baseModel=base_model, numClasses=5)
 
 model = Model(inputs=base_model.input, outputs=head)
 model.summary()
 
 print ('[INFO] FREEZING BASE MODEL LAYERS')
-for layer in base_model.layers:
+for layer in base_model.layers[15:]:
     layer.trainable = False
 
 #  put the model together
 model.compile(loss="categorical_crossentropy", optimizer=RMSprop(lr=0.001), metrics=["accuracy"])
-
-model.fit(train_ds,2, epochs=20, validation_data=validation_ds)
-
-predictions = model.predict(test_ds)
+print("FIT_1")
+model.fit(trainx,trainy, validation_data=(testx,testy), epochs=20, batch_size=20, verbose=1)
+print("Done FIT_1")
+predictions = model.predict(testy)
 print (predictions)
 
 
@@ -66,4 +72,4 @@ model.compile(loss="categorical_crossentropy", optimizer=SGD(lr=0.001), metrics=
 checkpoint = ModelCheckpoint('./test_model.hdf5', monitor="val_loss",save_best_only=True, verbose=1)
 callbacks = [checkpoint]
 
-model.fit(train_ds,2, epochs=100, validation_data=validation_ds, callbacks=callbacks)
+model.fit(trainx,trainy, validation_data=(testx,testy), epoche=100, batch_size=10, verbose=1)
