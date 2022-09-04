1f0d98fed9737f1a552a90b2df1fec3b3d363648
Triston Whitescarver <100805621+tristonw109@users.noreply.github.com>
2022-09-04 14:04:31 -0500

    reverting finetune_test.py

--- DL_Team/finetune/finetune_test.py
+++ DL_Team/finetune/finetune_test.py
@@ -1,75 +1,75 @@
 import numpy                      as np
 import tensorflow                 as tf
 import tensorflow_datasets        as tfds
-from   sklearn.preprocessing      import LabelBinarizer
 from   tensorflow.keras.callbacks import ModelCheckpoint
-from   keras.optimizers           import RMSprop
-from   keras.optimizers           import SGD
-from   newoutput_layer            import OutputLayers
-from   keras.layers               import Input
 from   tensorflow                 import keras
-from   keras.models               import Model
 
-
-print("LOADING DATASET")
-((trainx,trainy),(testx,testy)) = tfds.as_numpy(tfds.load(
-    "tf_flowers",
-    split=["train[:40]", "train[40%:60%]"],
-    batch_size=-1,
+(train_ds, validation_ds, test_ds) = tfds.load(
+    "cats_vs_dogs",
+    split=["train[:40%]", "train[40%:50%]","train[50%:60%]"],
     as_supervised=True
-))
+)
 
-trainx = np.resize(trainx, (240,240))
-trainy = np.resize(trainy, (240,240))
-testx = np.resize(testx, (240,240))
-testy = np.resize(testy, (240,240))
+size = (224,224)
 
-print(trainx.shape)
-print("Here")
-trainx = trainx.astype("float") / 255.0
-testx = testx.astype("float") / 255.0
-print("Here1")
-#lb = LabelBinarizer()
-#trainy = lb.fit_transform(trainy)
-#testy = lb.transform(testy)
-print("Here2")
+train_ds      = train_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+validation_ds = validation_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+test_ds      = test_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+
+batch_size = 32
+
+train_ds      = train_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+validation_ds = validation_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+test_ds       = test_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+
 data_augmentation = keras.Sequential(
     [keras.layers.RandomFlip("horizontal"), keras.layers.RandomRotation(0.1),]
 )
-print("Here3")
+
 base_model = keras.applications.VGG16(
     weights     = 'imagenet',
-    input_tensor = Input(shape=(240,240,3)),
+    input_shape = (224,224,3),
     include_top = False # do not include Imagenet classifer
 )
-print("Here4")
-# freeze the base model
-head = OutputLayers.build(D=256, baseModel=base_model, numClasses=5)
 
-model = Model(inputs=base_model.input, outputs=head)
+# freeze the base model
+base_model.trainable = False
+print("[INFO] FREEZEING LAYERS")
+
+inputs = keras.Input(shape=(224,224,3))
+x      = data_augmentation(inputs)
+
+scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+x           = scale_layer(x)
+
+x       = base_model(x, training=False)
+x       = keras.layers.GlobalAveragePooling2D()(x)
+x       = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+outputs = keras.layers.Dense(2)(x)
+model   = keras.Model(inputs, outputs)
+
 model.summary()
 
-print ('[INFO] FREEZING BASE MODEL LAYERS')
-for layer in base_model.layers[15:]:
-    layer.trainable = False
+# put the model together
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
 
-#  put the model together
-model.compile(loss="categorical_crossentropy", optimizer=RMSprop(lr=0.001), metrics=["accuracy"])
-print("FIT_1")
-model.fit(trainx,trainy, validation_data=(testx,testy), epochs=20, batch_size=20, verbose=1)
-print("Done FIT_1")
-predictions = model.predict(testy)
-print (predictions)
+model.fit(train_ds, epochs=20, validation_data=validation_ds)
 
-
+base_model.trainable = True
 print("[INFO] UNFREEZING LAYERS")
-for layer in base_model.layers:
-    layer.trainable = True
 
-model.compile(loss="categorical_crossentropy", optimizer=SGD(lr=0.001), metrics=["accuracy"])
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-5),  # Low learning rate to avoid over fitting
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
 
 # set up checkpointing
 checkpoint = ModelCheckpoint('./test_model.hdf5', monitor="val_loss",save_best_only=True, verbose=1)
 callbacks = [checkpoint]
 
-model.fit(trainx,trainy, validation_data=(testx,testy), epoche=100, batch_size=10, verbose=1)
+model.fit(train_ds, epochs=10, validation_data=validation_ds, callbacks=callbacks)
